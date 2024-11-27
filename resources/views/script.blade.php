1e3e0e2349be8f764ccca099db4228c02b079750
Laura Tonning <tonning@gmail.com>
2024-11-27 15:36:44 +0100

    Add new script

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,4 +1,8 @@
 @if(Astrogoat\Yotpo\Settings\YotpoSettings::isEnabled())
+    {{--  Yotpo v3 script  --}}
+    <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/{{ settings(Astrogoat\Yotpo\Settings\YotpoSettings::class, 'app_key') }}" async></script>
+
+    {{--  Yotpo legacy script  --}}
     <script type="text/javascript">
         (function e() {
             var e = document.createElement("script");
