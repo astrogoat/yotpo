43dfbc94a09c2678a4d295801fa203f17aab37ae
Laura Tonning <github@tonning.dev>
2025-05-16 10:30:46 +0200

    Be explicit

--- resources/views/components/reviews-widget.blade.php
+++ resources/views/components/reviews-widget.blade.php
@@ -1,11 +1,11 @@
-@props(['instanceId', 'product'])
+@props(['instanceId', 'productId', 'productTitle', 'productUrl'])
 
 <div
     class="yotpo-widget-instance"
     data-yotpo-instance-id="{{ $instanceId }}"
-    data-yotpo-product-id="{{ $product->shopify_id }}"
-    data-yotpo-name="{{ $product->title }}"
-    data-yotpo-url="{{ $product->getShowRoute() }}"
+    data-yotpo-product-id="{{ $productId }}"
+    data-yotpo-name="{{ $productTitle }}"
+    data-yotpo-url="{{ $productUrl }}"
 ></div>
 
 @pushonce('strata:frontend:end')
