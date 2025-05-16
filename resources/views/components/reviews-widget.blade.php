e489e66e7164b66aff94d8e8129739b64018ebad
Laura Tonning <github@tonning.dev>
2025-05-16 09:50:56 +0200

    Add product name and url

--- resources/views/components/reviews-widget.blade.php
+++ resources/views/components/reviews-widget.blade.php
@@ -1,9 +1,11 @@
-@props(['instanceId', 'productId'])
+@props(['instanceId', 'product'])
 
 <div
     class="yotpo-widget-instance"
     data-yotpo-instance-id="{{ $instanceId }}"
-    data-yotpo-product-id="{{ $productId }}"
+    data-yotpo-product-id="{{ $product->shopify_id }}"
+    data-yotpo-name="{{ $product->title }}"
+    data-yotpo-url="{{ $product->getShowRoute() }}"
 ></div>
 
 @pushonce('strata:frontend:end')
